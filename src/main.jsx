b2c9d3d974eefab178111cb912fe2a873a8ab4f3
newmomav <lipaavar@msn.com>
2023-11-22 12:25:48 +0100

    last updates with vecel main, archie, and variable colors

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
