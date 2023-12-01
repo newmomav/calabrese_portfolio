2402db6e0501c701015ae44ff10b404cb9982d30
newmomav <lipaavar@msn.com>
2023-12-01 15:18:13 +0100

    ok

--- src/components/sliderData.js
+++ src/components/sliderData.js
@@ -1,31 +1,85 @@
 export const sliderData = [
   {
+    src: '/ArchiveFotoskomprimiert/ceramic-candleholder-rosebud-bluegreen2.jpg',
+    alt: 'ceramic candleholder rosebud bluegreen',
+  },
+  {
+    src: '/ArchiveFotoskomprimiert/ceramic-claw-ringholder-pink.jpg',
+    alt: 'ceramic claw ringholder pink with bronze chain detail',
+  },
+
+  {
+    src: '/ArchiveFotoskomprimiert/stonewarebowlonthreelegs.jpg',
+    alt: 'stoneware bowl on three legs',
+  },
+
+  {
+    src: '/ArchiveFotoskomprimiert/mortadella-flower.gif',
+    alt: 'mortadella flower animated',
+  },
+  {
+    src: '/ArchiveFotoskomprimiert/ceramic-flower-plate.jpg',
+    alt: 'ceramic flower plate butteryellow',
+  },
+  {
+    src: '/ArchiveFotoskomprimiert/ceramic-ribbon-jeansglaze.jpg',
+    alt: 'ceramic ribbon with jeansglaze on dresser',
+  },
+
+  {
+    src: '/ArchiveFotoskomprimiert/stoneware-cup-whitebeige.jpg',
+    alt: 'stoneware cup white blue beige detail',
+  },
+  {
+    src: '/ArchiveFotoskomprimiert/ceramiccandleholderberrydetail.jpg',
+    alt: 'ceramic candleholder berry detail',
+  },
+  {
+    src: '/ArchiveFotoskomprimiert/stoneware-plate-ancient-graffiti5.jpg',
+    alt: 'stoneware plate ancient graffiti white blue',
+  },
+  {
+    src: '/ArchiveFotoskomprimiert/stoneware-plate-brown-beige.jpg',
+    alt: 'stoneware plate brown beige in front of blue wall',
+  },
+
+  {
+    src: '/ArchiveFotoskomprimiert/stoneware-bowls-tears-sgraffito.jpg',
+    alt: 'stoneware bowls tiny with tears sgraffito',
+  },
+  {
+    src: '/ArchiveFotoskomprimiert/stoneware-plate-ancient-graffiti1.jpg',
+    alt: 'stoneware plate ancient graffiti back',
+  },
+
+  {
+    src: '/ArchiveFotoskomprimiert/ceramic-wallpiece-burning-heart.jpg',
+    alt: 'ceramic wallpiece burning heart bubblewrap',
+  },
+  {
+    src: '/ArchiveFotoskomprimiert/stoneware-plate-detail.jpg',
+    alt: 'stoneware plate glaze detail',
+  },
+
+  {
     src: '/ArchiveFotoskomprimiert/ceramic-bowl-green.jpg',
     alt: 'ceramic bowl green baroque with pink worms in sink',
   },
   {
-    src: '/ArchiveFotoskomprimiert/ceramic-bowl-green2.jpg',
-    alt: 'ceramic bowl green baroque with pink worms',
+    src: '/ArchiveFotoskomprimiert/stoneware-cup-whiteblue2..jpg',
+    alt: 'stoneware cup white blue beige detail',
   },
   {
     src: '/ArchiveFotoskomprimiert/ceramic-candleholder-rosebud-berry.jpg',
     alt: 'ceramic candleholder rosebud berry',
   },
   {
-    src: '/ArchiveFotoskomprimiert/ceramic-candleholder-rosebud-bluegreen.jpg',
-    alt: 'ceramic candleholder rosebud bluegreen',
-  },
-  {
-    src: '/ArchiveFotoskomprimiert/ceramic-candleholder-rosebud-bluegreen-sideview.jpg',
-    alt: 'ceramic candleholder rosebud bluegreen sideview',
-  },
-  {
-    src: '/ArchiveFotoskomprimiert/ceramic-candleholder-rosebud-bluegreen2.jpg',
-    alt: 'ceramic candleholder rosebud bluegreen',
-  },
-  {
     src: '/ArchiveFotoskomprimiert/ceramic-claw-ringholder-beige.jpg',
     alt: 'ceramic claw ringholder beige',
+  },
+  {
+    src: '/ArchiveFotoskomprimiert/stonewarebowlbluewhitedownsideup.jpg',
+    alt: 'stoneware bowl blue white downsideup',
   },
   {
     src: '/ArchiveFotoskomprimiert/ceramic-claw-ringholder-beige-worms.jpg',
@@ -35,10 +89,7 @@ export const sliderData = [
     src: '/ArchiveFotoskomprimiert/ceramic-claw-ringholder-brown.jpg',
     alt: 'ceramic claw ringholder brown on frozen ivy',
   },
-  {
-    src: '/ArchiveFotoskomprimiert/ceramic-claw-ringholder-brown-worms.jpg',
-    alt: 'ceramic claw ringholder brown with pink worms',
-  },
+
   {
     src: '/ArchiveFotoskomprimiert/ceramic-claw-ringholder-green-business.jpg',
     alt: 'ceramic claw ringholder green business',
@@ -47,62 +98,60 @@ export const sliderData = [
     src: '/ArchiveFotoskomprimiert/ceramic-claw-ringholder-green-flames.jpg',
     alt: 'ceramic claw ringholder green with flames',
   },
-  {
-    src: '/ArchiveFotoskomprimiert/ceramic-claw-ringholder-green-flames2.jpg',
-    alt: 'ceramic claw ringholder green with flames in front of white background',
-  },
+
   {
     src: '/ArchiveFotoskomprimiert/ceramic-claw-ringholder-green-worms.jpg',
     alt: 'ceramic claw ringholder green flesh worm detail',
   },
+
   {
-    src: '/ArchiveFotoskomprimiert/ceramic-claw-ringholder-pink.jpg',
-    alt: 'ceramic claw ringholder pink with bronze chain detail',
+    src: '/ArchiveFotoskomprimiert/stoneware-plate-ancient-graffiti.jpg',
+    alt: 'stoneware plate ancient graffiti blue beige full',
   },
-  {
-    src: '/ArchiveFotoskomprimiert/ceramic-flower-plate.jpg',
-    alt: 'ceramic flower plate butteryellow',
-  },
+
   {
     src: '/ArchiveFotoskomprimiert/ceramic-piece-mortadella-flower.jpg',
     alt: 'ceramic piece mortadella flower in front of aquarium sideview',
+  },
+
+  {
+    src: '/ArchiveFotoskomprimiert/ceramic-ringholder-claw-white1.jpg',
+    alt: 'ceramic claw ringholder in white detail',
   },
   {
     src: '/ArchiveFotoskomprimiert/ceramic-piece-mortadella-flower2.jpg',
     alt: 'ceramic piece mortadella flower in front of aquarium',
   },
   {
-    src: '/ArchiveFotoskomprimiert/ceramic-ribbon-jeansglaze.jpg',
-    alt: 'ceramic ribbon with jeansglaze on dresser',
-  },
-  {
-    src: '/ArchiveFotoskomprimiert/ceramic-ringholder-claw-white1.jpg',
-    alt: 'ceramic claw ringholder in white detail',
-  },
-  {
     src: '/ArchiveFotoskomprimiert/Kralle-gif.gif',
     alt: 'ceramic claw ringholder animates',
   },
+
   {
-    src: '/ArchiveFotoskomprimiert/ceramic-ringholder-claw-white-worm.jpg',
-    alt: 'ceramic claw ringholder in white wormdetail',
+    src: '/ArchiveFotoskomprimiert/ceramic-bowl-green2.jpg',
+    alt: 'ceramic bowl green baroque with pink worms',
   },
   {
-    src: '/ArchiveFotoskomprimiert/ceramic-wallpiece-burning-heart.jpg',
-    alt: 'ceramic wallpiece burning heart bubblewrap',
+    src: '/ArchiveFotoskomprimiert/ceramic-claw-ringholder-green-flames2.jpg',
+    alt: 'ceramic claw ringholder green with flames in front of white background',
   },
   {
     src: '/ArchiveFotoskomprimiert/ceramic-wallpiece-heart.jpg',
     alt: 'ceramic wallpiece heart in chains espressobrown',
   },
   {
+    src: '/ArchiveFotoskomprimiert/ceramic-candleholder-rosebud-bluegreen.jpg',
+    alt: 'ceramic candleholder rosebud bluegreen',
+  },
+  {
+    src: '/ArchiveFotoskomprimiert/ceramic-claw-ringholder-brown-worms.jpg',
+    alt: 'ceramic claw ringholder brown with pink worms',
+  },
+  {
     src: '/ArchiveFotoskomprimiert/ceramicbowlbluewhite.jpg',
     alt: 'ceramic bowl blue white',
   },
-  {
-    src: '/ArchiveFotoskomprimiert/ceramiccandleholderberrydetail.jpg',
-    alt: 'ceramic candleholder berry detail',
-  },
+
   {
     src: '/ArchiveFotoskomprimiert/ceramicclawringholdergreen.jpg',
     alt: 'ceramic claw ringholder green',
@@ -116,9 +165,19 @@ export const sliderData = [
     alt: 'ceramic rosebud candleholder berry',
   },
   {
-    src: '/ArchiveFotoskomprimiert/mortadella-flower.gif',
-    alt: 'mortadella flower animated',
+    src: '/ArchiveFotoskomprimiert/stoneware-bowl-white-beige-blue2.jpg',
+    alt: 'stoneware bowl white beige blue',
   },
+
+  {
+    src: '/ArchiveFotoskomprimiert/ceramic-candleholder-rosebud-bluegreen-sideview.jpg',
+    alt: 'ceramic candleholder rosebud bluegreen sideview',
+  },
+  {
+    src: '/ArchiveFotoskomprimiert/stoneware-cup-jeans4.jpg',
+    alt: 'stoneware cup jeans washed out',
+  },
+
   {
     src: '/ArchiveFotoskomprimiert/stoneware-bowl-beige-blue-white.jpg',
     alt: 'stoneware bowl beige blue white detail',
@@ -128,37 +187,28 @@ export const sliderData = [
     alt: 'stoneware bowl on three legs held by hand',
   },
   {
-    src: '/ArchiveFotoskomprimiert/stoneware-bowl-three-legs2.jpg',
-    alt: 'stoneware bowl on three short legs brown blue',
-  },
-  {
-    src: '/ArchiveFotoskomprimiert/stoneware-bowl-white-beige-blue.jpg',
-    alt: 'stoneware bowl white beige blue downsideup',
-  },
-  {
-    src: '/ArchiveFotoskomprimiert/stoneware-bowl-white-beige-blue2.jpg',
-    alt: 'stoneware bowl white beige blue',
-  },
-  {
-    src: '/ArchiveFotoskomprimiert/stoneware-bowl-white-beige-blue3.jpg',
-    alt: 'stoneware bowl white beige blue held by hand',
-  },
-  {
     src: '/ArchiveFotoskomprimiert/stoneware-bowl-three-short-legs.jpg',
     alt: 'stoneware bowl on three short legs detail',
   },
   {
-    src: '/ArchiveFotoskomprimiert/stoneware-cup-whitebeige.jpg',
-    alt: 'stoneware cup white blue beige detail',
+    src: '/ArchiveFotoskomprimiert/stoneware-bowl-three-legs2.jpg',
+    alt: 'stoneware bowl on three short legs brown blue',
   },
+  {
+    src: '/ArchiveFotoskomprimiert/ceramic-ringholder-claw-white-worm.jpg',
+    alt: 'ceramic claw ringholder in white wormdetail',
+  },
+
+  {
+    src: '/ArchiveFotoskomprimiert/stoneware-bowl-white-beige-blue.jpg',
+    alt: 'stoneware bowl white beige blue downsideup',
+  },
+
   {
     src: '/ArchiveFotoskomprimiert/stoneware-cup-whiteblue.jpg',
     alt: 'stoneware cup white blue beige',
   },
-  {
-    src: '/ArchiveFotoskomprimiert/stoneware-cup-whiteblue2..jpg',
-    alt: 'stoneware cup white blue beige detail',
-  },
+
   {
     src: '/ArchiveFotoskomprimiert/stoneware-cup-whiteblue3.jpg',
     alt: 'stoneware cup white blue beige detail',
@@ -167,22 +217,16 @@ export const sliderData = [
     src: '/ArchiveFotoskomprimiert/stoneware-cup-jeans3.jpg',
     alt: 'stoneware cup jeans washed out',
   },
-  {
-    src: '/ArchiveFotoskomprimiert/stoneware-cup-jeans4.jpg',
-    alt: 'stoneware cup jeans washed out',
-  },
-  {
-    src: '/ArchiveFotoskomprimiert/stoneware-plate-ancient-graffiti1.jpg',
-    alt: 'stoneware plate ancient graffiti back',
-  },
-  {
-    src: '/ArchiveFotoskomprimiert/stoneware-plate-ancient-graffiti.jpg',
-    alt: 'stoneware plate ancient graffiti blue beige full',
-  },
+
   {
     src: '/ArchiveFotoskomprimiert/stoneware-plate-ancient-graffiti2.jpg',
     alt: 'stoneware plate ancient graffiti blue beige full2',
   },
+  {
+    src: '/ArchiveFotoskomprimiert/stoneware-bowl-white-beige-blue3.jpg',
+    alt: 'stoneware bowl white beige blue held by hand',
+  },
+
   {
     src: '/ArchiveFotoskomprimiert/stoneware-plate-ancient-graffiti3.jpg',
     alt: 'stoneware plate ancient graffiti detail',
@@ -191,26 +235,7 @@ export const sliderData = [
     src: '/ArchiveFotoskomprimiert/stoneware-plate-ancient-graffiti4.jpg',
     alt: 'stoneware plate ancient graffiti front',
   },
-  {
-    src: '/ArchiveFotoskomprimiert/stoneware-plate-ancient-graffiti5.jpg',
-    alt: 'stoneware plate ancient graffiti white blue',
-  },
-  {
-    src: '/ArchiveFotoskomprimiert/stoneware-plate-brown-beige.jpg',
-    alt: 'stoneware plate brown beige in front of blue wall',
-  },
-  {
-    src: '/ArchiveFotoskomprimiert/stoneware-plate-detail.jpg',
-    alt: 'stoneware plate glaze detail',
-  },
-  {
-    src: '/ArchiveFotoskomprimiert/stonewarebowlbluewhitedownsideup.jpg',
-    alt: 'stoneware bowl blue white downsideup',
-  },
-  {
-    src: '/ArchiveFotoskomprimiert/stonewarebowlonthreelegs.jpg',
-    alt: 'stoneware bowl on three legs',
-  },
+
   {
     src: '/ArchiveFotoskomprimiert/stonewarebowlbluewhite.jpg',
     alt: 'stoneware bowl blue white2',
@@ -218,9 +243,5 @@ export const sliderData = [
   {
     src: '/ArchiveFotoskomprimiert/stonewarebowlwithapple.jpg',
     alt: 'stoneware bowl blue white with apple',
-  },
-  {
-    src: '/ArchiveFotoskomprimiert/stoneware-bowls-tears-sgraffito.jpg',
-    alt: 'stoneware bowls tiny with tears sgraffito',
   },
 ];
