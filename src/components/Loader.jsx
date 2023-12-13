bd42957eea2bb82dd905e04e01d9dae01d8a4ebf
newmomav <lipaavar@msn.com>
2023-12-13 13:29:46 +0100

    calabrese loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -6,8 +6,14 @@ const Loader = () => {
         className=" fixed top-0 left-0 h-screen w-screen z-50 flex items-center justify-center fade-out"
       >
         <h1 className="loaderLogo">
-          123456<span className="e">7</span>
-          <span className="se">89</span>
+          1<span className="a">2</span>
+          <span className="l">3</span>
+          <span className="a2">4</span>
+          <span className="b">5</span>
+          <span className="r">6</span>
+          <span className="e">7</span>
+          <span className="s">8</span>
+          <span className="e2">9</span>
         </h1>
       </div>
     </>
